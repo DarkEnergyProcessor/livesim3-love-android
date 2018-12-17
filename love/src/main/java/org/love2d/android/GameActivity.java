8a48a6a4b90d97afb8e8040a8d6b0a95902d6e89
MikuAuahDark <auahdark687291@gmail.com>
2018-12-17 22:28:18 +0800

    Fix compilation error

--- love/src/main/java/org/love2d/android/GameActivity.java
+++ love/src/main/java/org/love2d/android/GameActivity.java
@@ -17,6 +17,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Vibrator;
