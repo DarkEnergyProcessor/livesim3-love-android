983fa067a5b70af56118d53c812ee5f8cf28fe0b
MikuAuahDark <auahdark687291@gmail.com>
2018-12-17 21:56:37 +0800

    Forgot to readd android.mk lmao

---
+++ love/src/jni/openal-soft-1.16.0/Android.mk
@@ -0,0 +1,9 @@
+# CMake is used to build the prebuilt binary for Android
+# Base is OpenAL-soft 1.19.1
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libopenal
+LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libopenal.so
+include $(PREBUILT_SHARED_LIBRARY)
