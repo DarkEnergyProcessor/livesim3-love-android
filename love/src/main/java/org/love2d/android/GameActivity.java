e8eb54e2ffb8c9deea1da3b0cb3362c2947ca615
MikuAuahDark <auahdark687291@gmail.com>
2019-02-10 09:59:55 +0800

    https://bitbucket.org/MartinFelis/love-android-sdl2/pull-requests/17

--- love/src/main/java/org/love2d/android/GameActivity.java
+++ love/src/main/java/org/love2d/android/GameActivity.java
@@ -17,7 +17,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Vibrator;
@@ -37,6 +36,10 @@ public class GameActivity extends SDLActivity {
     public static final int EXTERNAL_STORAGE_REQUEST_CODE = 1;
     private static boolean immersiveActive = false;
     private static boolean mustCacheArchive = false;
+    public int safeAreaTop = 0;
+    public int safeAreaLeft = 0;
+    public int safeAreaBottom = 0;
+    public int safeAreaRight = 0;
 
     @Override
     protected String[] getLibraries() {
@@ -53,7 +56,7 @@ public class GameActivity extends SDLActivity {
     @Keep
     public int getAudioSMP()
     {
-        if (Build.VERSION.SDK_INT >= 17)
+        if (android.os.Build.VERSION.SDK_INT >= 17)
         {
             AudioManager a = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
             int b = Integer.parseInt(a.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER));
@@ -66,7 +69,7 @@ public class GameActivity extends SDLActivity {
     @Keep
     public int getAudioFreq()
     {
-        if (Build.VERSION.SDK_INT >= 17)
+        if (android.os.Build.VERSION.SDK_INT >= 17)
         {
             AudioManager a = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
             int b = Integer.parseInt(a.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE));
@@ -98,6 +101,10 @@ public class GameActivity extends SDLActivity {
         nativeSetenv("LLA_FREQUENCY", String.valueOf(getAudioFreq()));
         nativeSetenv("LLA_IS_SET", "1");
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        if (android.os.Build.VERSION.SDK_INT >= 28) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
     }
 
     @Override
@@ -124,6 +131,8 @@ public class GameActivity extends SDLActivity {
                 } else {
                     gamePath = game.getPath();
                 }
+            // FIXME: Add support for "content://" URI, which is mandatory in Android Nougat
+            // as using "file://" protocol in that (and later) version is forbidden!
             } else {
                 Log.e("GameActivity", "Unsupported scheme: '" + game.getScheme() + "'.");
 
@@ -201,13 +210,19 @@ public class GameActivity extends SDLActivity {
         super.onResume();
 
         if (immersiveActive) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+            if (android.os.Build.VERSION.SDK_INT >= 16) {
+                flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                         View.SYSTEM_UI_FLAG_FULLSCREEN;
+            }
+            if (android.os.Build.VERSION.SDK_INT >= 19) {
+                flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+
+            getWindow().getDecorView().setSystemUiVisibility(flags);
         }
     }
 
@@ -229,17 +244,22 @@ public class GameActivity extends SDLActivity {
                 public void run() {
                     synchronized (lock) {
                         if (immersive_enabled) {
+                            int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+                            if (android.os.Build.VERSION.SDK_INT >= 16) {
+                                flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                         View.SYSTEM_UI_FLAG_FULLSCREEN;
+                            }
+                            if (android.os.Build.VERSION.SDK_INT >= 19) {
+                                flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                            }
+                            getWindow().getDecorView().setSystemUiVisibility(flags);
+                        } else if (android.os.Build.VERSION.SDK_INT >= 16) {
                             getWindow().getDecorView().setSystemUiVisibility(
-                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-                        } else {
-                            getWindow().getDecorView().setSystemUiVisibility(
-                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             );
                         }
 
@@ -421,5 +441,22 @@ public class GameActivity extends SDLActivity {
         }
 
         return ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Keep
+    public boolean initializeSafeArea() {
+        if (android.os.Build.VERSION.SDK_INT >= 28) {
+            DisplayCutout cutout = getWindow().getDecorView().getRootWindowInsets().getDisplayCutout();
+
+            if (cutout != null) {
+                safeAreaTop = cutout.getSafeInsetTop();
+                safeAreaLeft = cutout.getSafeInsetLeft();
+                safeAreaBottom = cutout.getSafeInsetBottom();
+                safeAreaRight = cutout.getSafeInsetRight();
+                return true;
+            }
+        }
+
+        return false;
     }
 }
