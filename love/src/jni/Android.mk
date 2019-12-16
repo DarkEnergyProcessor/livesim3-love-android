4777c9657c76931ac22d50e009c3eaf7376116e7
Miku AuahDark <auahdark687291@gmail.com>
2019-12-16 20:44:21 +0800

    Pull latest LJ, ls3love, and (upstream) love-android

--- love/src/jni/Android.mk
+++ love/src/jni/Android.mk
@@ -1,5 +1,10 @@
-IS_NDK_R17 := $(shell python $(call my-dir)/detect_ndkrel.py $(NDK_ROOT)/source.properties 17)
-IS_ANDROID_21 := $(shell python $(call my-dir)/detect_androidapi.py $(TARGET_PLATFORM) 21)
-SDL_PATH := $(call my-dir)/SDL2-2.0.9
+LOVE_JNI_DIR := $(call my-dir)
+IS_NDK_R17 := $(shell python ${LOVE_JNI_DIR}/detect_ndkrel.py $(NDK_ROOT)/source.properties 17)
+IS_ANDROID_21 := $(shell python ${LOVE_JNI_DIR}/detect_androidapi.py $(TARGET_PLATFORM) 21)
+HAS_LOVE := $(shell python ${LOVE_JNI_DIR}/detect_love.py ${LOVE_JNI_DIR})
+
+ifneq (${HAS_LOVE},yes)
+    $(error Missing LOVE. Make sure to initialize the submodule correctly!)
+endif
 
 include $(call all-subdir-makefiles)
