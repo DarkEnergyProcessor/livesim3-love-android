a999dc98a0b7ac938e12a6af012236465b5fd270
Miku AuahDark <auahdark687291@gmail.com>
2020-06-19 12:49:19 +0800

    Update build instructions readme.
    Add -D_CRT_SECURE_NO_WARNINGS to HOST_CFLAGS because MSVC's libc >:(

--- love/src/jni/LuaJIT-2.1/build_instructions.bat
+++ love/src/jni/LuaJIT-2.1/build_instructions.bat
@@ -1,6 +1,10 @@
 rem Build instruction assume using WSL + Clang for Windows (with MSVC x86+x64 toolset for -m32 switch)
 rem This assume NDK r19 or later. See https://github.com/LuaJIT/LuaJIT/issues/477 for more information.
 rem Make sure the prebuilt is already in your PATH environment variable.
+rem Also make sure lj_ircall.h.patch is applied to src/lj_ircall.h. In this repo, the patch has been
+rem applied. However if you updated LuaJIT from the latest v2.1 branch, you may need to reapply it.
+rem If you're confused which one should be added your path:
+rem <NDK_ROOT>\toolchains\llvm\prebuilt\windows-x86_64\bin
 
 mkdir android\arm64-v8a
 mkdir android\armeabi-v7a
@@ -13,7 +17,7 @@ rem ARMv8
 if not exist android\arm64-v8a\libluajit.a (
 	wsl make clean
 	if "%ERRORLEVEL%" == "1" goto :error
-	wsl make HOST_LUA=luajit HOST_CC=clang.exe CC=clang CROSS=aarch64-linux-android- STATIC_CC=aarch64-linux-android21-clang "DYNAMIC_CC=aarch64-linux-android21-clang -fPIC" "TARGET_AR=aarch64-linux-android-ar.exe rcus" TARGET_LD=aarch64-linux-android21-clang TARGET_STRIP=aarch64-linux-android-strip.exe amalg
+	wsl make HOST_LUA=luajit HOST_CC=clang.exe HOST_CFLAGS=-D_CRT_SECURE_NO_WARNINGS CC=clang CROSS=aarch64-linux-android- STATIC_CC=aarch64-linux-android21-clang "DYNAMIC_CC=aarch64-linux-android21-clang -fPIC" "TARGET_AR=aarch64-linux-android-ar.exe rcus" TARGET_LD=aarch64-linux-android21-clang TARGET_STRIP=aarch64-linux-android-strip.exe amalg
 	if "%ERRORLEVEL%" == "1" goto :error
 	copy src\libluajit.a android\arm64-v8a\libluajit.a
 	if "%ERRORLEVEL%" == "1" goto :error
@@ -23,7 +27,7 @@ rem ARMv7
 if not exist android\armeabi-v7a\libluajit.a (
 	wsl make clean
 	if "%ERRORLEVEL%" == "1" goto :error
-	wsl make HOST_LUA=luajit "HOST_CC=clang.exe -m32" CC=clang CROSS=arm-linux-android- STATIC_CC=armv7a-linux-androideabi16-clang "DYNAMIC_CC=armv7a-linux-androideabi16-clang -fPIC" "TARGET_AR=arm-linux-androideabi-ar.exe rcus" TARGET_LD=armv7a-linux-androideabi16-clang TARGET_STRIP=arm-linux-androideabi-strip.exe amalg
+	wsl make HOST_LUA=luajit "HOST_CC=clang.exe -m32" HOST_CFLAGS=-D_CRT_SECURE_NO_WARNINGS CC=clang CROSS=arm-linux-android- STATIC_CC=armv7a-linux-androideabi16-clang "DYNAMIC_CC=armv7a-linux-androideabi16-clang -fPIC" "TARGET_AR=arm-linux-androideabi-ar.exe rcus" TARGET_LD=armv7a-linux-androideabi16-clang TARGET_STRIP=arm-linux-androideabi-strip.exe amalg
 	if "%ERRORLEVEL%" == "1" goto :error
 	copy src\libluajit.a android\armeabi-v7a\libluajit.a
 	if "%ERRORLEVEL%" == "1" goto :error
@@ -33,7 +37,7 @@ rem x86
 if not exist android\x86\libluajit.a (
 	wsl make clean
 	if "%ERRORLEVEL%" == "1" goto :error
-	wsl make HOST_LUA=luajit "HOST_CC=clang.exe -m32" CC=clang CROSS=i686-linux-android- STATIC_CC=i686-linux-android16-clang "DYNAMIC_CC=i686-linux-android16-clang -fPIC" "TARGET_AR=i686-linux-android-ar.exe rcus" TARGET_LD=i686-linux-android16-clang TARGET_STRIP=i686-linux-android-strip.exe amalg
+	wsl make HOST_LUA=luajit "HOST_CC=clang.exe -m32" HOST_CFLAGS=-D_CRT_SECURE_NO_WARNINGS CC=clang CROSS=i686-linux-android- STATIC_CC=i686-linux-android16-clang "DYNAMIC_CC=i686-linux-android16-clang -fPIC" "TARGET_AR=i686-linux-android-ar.exe rcus" TARGET_LD=i686-linux-android16-clang TARGET_STRIP=i686-linux-android-strip.exe amalg
 	if "%ERRORLEVEL%" == "1" goto :error
 	copy src\libluajit.a android\x86\libluajit.a
 	if "%ERRORLEVEL%" == "1" goto :error
